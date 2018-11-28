24c91710d18b1f6a87c7c97b560796c05fee83fa
Rosales Rosales Diana <rosalesrd2018@dci25-171.ugto.mx>
2018-11-28 10:36:49 -0600

    actualice

--- proyecto/proyecto.c
+++ proyecto/proyecto.c
@@ -1,3 +1,25 @@
+//Proyecto final de Programación Básica 2018
+//Cadenas de Markov Chain Montecarlo (MCMC) con el método de Metropolis
+//Participantes: Diana Hernández Bustos y Diana Rosales Rosales
+//Este trabajo se comenzó el Miércoles 28 de Noviembre
+
 #include<stdio.h>
 #include<math.h>
+int main(){
 
+int i=0,N;
+N=10;
+float x[N],y[N],error[N];
+
+
+FILE *fp; //Para abrir documento para leer archivos iniciales
+
+fp=fopen("datos.txt","r");
+
+for(i=0;i<N;i++){
+	fscanf(fp,"%f %f %f",&x[i],&y[i],&error[i]);
+	printf("%f %f %f \n",x[i],y[i],error[i]);
+		}
+
+return 0;
+}
