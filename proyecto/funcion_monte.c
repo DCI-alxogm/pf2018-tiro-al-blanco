c55779b44b53d92d3b1f3c68d6ec6b09b0134388
Diana Ailed Hernandez <diana1412hb@gmail.com>
2018-12-08 00:15:26 -0600

    Subí gráficas

--- proyecto/funcion_monte.c
+++ proyecto/funcion_monte.c
@@ -5,34 +5,49 @@
 
 void calculo_monte(int N,  int x[], float y[], float o[]){ //Esta función tiene elementos de entrada pero no de salida
 
-	float *m0, *b0, pend, ord, mn, bn, Pmn, mmin, mmax, bmin, bmax, num, sigmam, sigmab;//Hacemos declaración de variables de tipo decimal
-	int i, L; //L es las veces que quiere el usuario que se corra el programa para calcular m y b que mejor se ajustan a los datos experimentales
+	float *m0, *b0, pend[4], ord[4], mn, bn, Pmn, mmin, mmax, bmin, bmax, num, sigmam, sigmab, pendi, orde;//Hacemos declaración de variables de tipo decimal
+	int i, L, count=0, j; //L es las veces que quiere el usuario que se corra el programa para calcular m y b que mejor se ajustan a los datos experimentales
 	double lnpl; //lnpl, es el logaritmo de likelihood
 	FILE *fp2; //Se hace la declaración del archivo de escritura donde van a estar nuestros resultados
 	FILE *fp3; //Se hace la declaración del archivo de lectura donde se encuentran los parámetros para que funcione este método
 
 	fp3=fopen("parametros.txt","r"); //Se abre el archivo de lectura que contiene los parámetros
-		fscanf(fp3, "%f %f %f %f", &pend, &ord, &sigmam, &sigmab); //SE obtienen datos como "m" y "b" máximas, mínimas e iniciales, así cómo la cantidad de dispersión que se quiere que el archivo dé a los valores
-	 	fscanf(fp3, "%f %f %f %f", &mmin, &mmax, &bmin, &bmax);
-		fscanf(fp3, "%i", &L);
-	fclose(fp3); //Se cierra el archivo de lectura
+		for(j=0; j<4; j++){
+			if(count==0){
+				fscanf(fp3, "%f %f", &sigmam, &sigmab); //SE obtienen datos como "m" y "b" máximas, mínimas e iniciales, así cómo la cantidad de dispersión que se quiere que el archivo dé a los valores
+	 			fscanf(fp3, "%f %f %f %f", &mmin, &mmax, &bmin, &bmax);
+				fscanf(fp3, "%i", &L);
+				fscanf(fp3, "%f %f", &pend[j], &ord[j]);
+			}
+
+			else{
+				fscanf(fp3, "%f %f", &pend[j], &ord[j]);
+			}
+
+			count++;
+			
+			m0=(float*) malloc((L+10)*sizeof(int));//Se le ofrece un espacio reservado en la memoria a todos aquellos m0 y b0 que van a salir con el FOR, el cual corre "L" veces, pero más las pendientes y ordenadas iniciales puede a llegar a ser más.
+			b0=(float*) malloc((L+10)*sizeof(int));//Se le ofrece un espacio reservado en la memoria a todos aquellos m0 y b0 que van a salir con el FOR, el cual corre "L" veces, pero más las pendientes y ordenadas iniciales puede a llegar a ser más
+
+			pendi=pend[j];
+			orde=ord[j];
+
+			m0=&pendi; //Se le asigna un valor al apuntador de m0 y b0
+			b0=&orde;
+
+			char buffer [32]; //SE utiliza una variable de tipo char que tenga varios elementos (los diferentes nombres) de mis archivos a crear
+			snprintf(buffer, sizeof(char)*32, "Res_cadena%i.txt",j+1); //Asigno un nombre a cada uno de los archivos que se van a crear, los cuales van a tener el nombre según las veces que se haya hecho la iteración.
 	
-	m0=(float*) malloc((L+10)*sizeof(int));//Se le ofrece un espacio reservado en la memoria a todos aquellos m0 y b0 que van a salir con el FOR, el cual corre "L" veces, pero más las pendientes y ordenadas iniciales puede a llegar a ser más.
-	b0=(float*) malloc((L+10)*sizeof(int));//Se le ofrece un espacio reservado en la memoria a todos aquellos m0 y b0 que van a salir con el FOR, el cual corre "L" veces, pero más las pendientes y ordenadas iniciales puede a llegar a ser más.
+			fp2=fopen(buffer,"w"); //Se abre el archivo de escritura con el nombre de "Resultados"	
 
-	m0=&pend; //Se le asigna un valor al apuntador de m0 y b0
-	b0=&ord;
-
-	fp2=fopen("Resultados.txt","w"); //Se abre el archivo de escritura con el nombre de "Resultados"	
-
-	for(i=0; i<=L; i++){ //Se hace un FOR que corra "L" veces, el cual fue definido en el archivo de parámetros, de 1 en 1
-		lnpl= likelihood(*m0, *b0, N, x, y, o); //Se llama a la función likelihood (que tiene 6 elementos de entrada y 1 de salida) para que regrese el valor del logaritmo de likelihood
+			for(i=0; i<=L; i++){ //Se hace un FOR que corra "L" veces, el cual fue definido en el archivo de parámetros, de 1 en 1
+				lnpl= likelihood(*m0, *b0, N, x, y, o); //Se llama a la función likelihood (que tiene 6 elementos de entrada y 1 de salida) para que regrese el valor del logaritmo de likelihood
 		
-		mn = num_gauss(*m0, sigmam);//Se llama a la función num_gauss(que tiene 2 elementos de entrada) para que regrese al programa 1 número aleatorio para la pendiente que no se pase de sigmam 
-		bn = num_gauss(*b0, sigmab);//Se llama a la función num_gauss(que tiene 2 elementos de entrada) para que regrese al programa 1 número aleatorio para la ordenada al origen que no se pase de sigmab 
+				mn = num_gauss(*m0, sigmam);//Se llama a la función num_gauss(que tiene 2 elementos de entrada) para que regrese al programa 1 número aleatorio para la pendiente que no se pase de sigmam 
+				bn = num_gauss(*b0, sigmab);//Se llama a la función num_gauss(que tiene 2 elementos de entrada) para que regrese al programa 1 número aleatorio para la ordenada al origen que no se pase de sigmab 
 		
-			if(mmin<mn<mmax && bmin<bn<bmax){ //Si la nueva pendiente y la nueva ordenada no se pasan de los parátreos establecidos en los datos de parámetros, entonces:
-				Pmn=likelihood(mn, bn, N, x, y, o); //Se calcula la probabilidad de la nueva pendiente y se llama a esta función para que logre su objetivo
+				if(mmin<mn<mmax && bmin<bn<bmax){ //Si la nueva pendiente y la nueva ordenada no se pasan de los parátreos establecidos en los datos de parámetros, entonces:
+					Pmn=likelihood(mn, bn, N, x, y, o); //Se calcula la probabilidad de la nueva pendiente y se llama a esta función para que logre su objetivo
 									
 					if(Pmn > lnpl){ //Si el logaritmo de mn es mayor al log de likelihood (de la pendiente anterior), entonces:
 						//Se iguala la pendiente y la ordenada anteriores a las nuevas
@@ -50,12 +65,14 @@ void calculo_monte(int N,  int x[], float y[], float o[]){ //Esta función tiene
 							
 						}
 					}
-			}
+				}
 
-	fprintf(fp2, "%f \t %f \t %f \n",lnpl, *m0, *b0); //Se imprimen los resultados del logaritmo de likelihood, la pendiente y la ordenada que sí cumplieron con todos los requisitos
+		fprintf(fp2, "%f \t %f \t %f \n",lnpl, *m0, *b0); //Se imprimen los resultados del logaritmo de likelihood, la pendiente y la ordenada que sí cumplieron con todos los requisitos
 	
-	}
-	fclose(fp2); //Se cierra el archivo de escritura
+		}
+		fclose(fp2); //Se cierra el archivo de escritura
+		}
+		fclose(fp3); //Se cierra el archivo de lectura
 	
 } //Se cierra esta función
 
--- proyecto/otras_funciones.c
+++ proyecto/otras_funciones.c
@@ -1,44 +1,35 @@
-#include<stdio.h>
+#include<stdio.h> //SE hace la declaración de todas las librerías que se van a usar en el programa
 #include<math.h>
 #include<stdlib.h> 
-#include "funciones.h"
+#include "funciones.h" //Se incluye la librería creada por nosotras mismas que contiene la declaración de todas las funciones utilizadas
 
-float likelihood(float m0, float b0, int N, int x[], float y[], float o[]){ 
-	float ymod[N], aux, sumatoria=0, lnpl;
+float likelihood(float m0, float b0, int N, int x[], float y[], float o[]){ //Se abre la función likelihood, que tiene estos elementos de entrada y uno de salida, que viene siendo el logaritmo de likelihood (In de la probabilidad)
+	float ymod[N], aux, sumatoria=0, lnpl; //SE hace la declaración de las variables utilizadas en esta función
 	int j;
  
-		for(j=0;j<N;j++){ //SE calcula el logaritmo de likelihood
-			ymod[j]=m0*x[j]+b0;
-			aux=(pow((y[j]-ymod[j]),2))-(pow(o[j],2));
-			sumatoria+=aux;		
+		for(j=0;j<N;j++){ //Se calcula el logaritmo de likelihood
+			ymod[j]=m0*x[j]+b0; //Primero se obtiene un arreglo de una función (y) con la pendiente y la ordenada de entrada 
+			aux=(pow((y[j]-ymod[j]),2))-(pow(o[j],2)); //Después se hace este cálculo que reemplaza a un ajuste de mínimos cuadrados
+			sumatoria+=aux; //Se hace una sumatoria de los cálculos hechos		
 		}	
-		lnpl=(-0.5)*sumatoria;
+		lnpl=(-0.5)*sumatoria; //Luego, se saca el logaritmo de likelihood
 
-	return lnpl;
-}
+	return lnpl; //Se regresa el logaritmo natural obtenido
+}//Se cierra esta función
 
 
-double num_gauss(double x, double y) {
-	int aux2 = 0;
-	double spare;
-
-	if(aux2) {
-		aux2 = 0;
-		return x + y * spare;
-
-	}
-
-	aux2 = 1;
-	double u, v, s;
+double num_gauss(double x, double y) { //Se abre esta función para obtener 2 números al asar por medio de una distribución gaussiana, la cual, sólo tiene elementos de entrada la pendiente original y la sigma; o bien, la ordenada al origen y su repectiva sigma
+	//Se hace la declaración de las variables
+	double aux, u, v, s;
 	do {
-		u = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
+		u = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0; //Se escogen 2 números al asar entre 0 y 1
 		v = (rand() / ((double) RAND_MAX)) * 2.0 - 1.0;
-		s = u * u + v * v;
+		s = u * u + v * v; //Se calcula s
 	}
 
-	while( (s >= 1.0) || (s == 0.0) );
-		s = sqrt(-2.0 * log(s) / s);
-		spare = v * s;
-		return x + y * u * s;
+	while( (s >= 1.0) || (s == 0.0) ); //Siempre que s sea menor a 1 y mayor a 0
+		s = sqrt(-2.0 * log(s) / s); //Se hace esta operación para obtener mNEW y bNEW
+		aux = v * s;
+		return x + y * u * s; //Se regresa el valor de mNEW y bNEW
 	
-}
+} //Se cierra esta función
